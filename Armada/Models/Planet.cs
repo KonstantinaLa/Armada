66b26d914e4047494c58ab537217858f73b0b087
Kostas <Kwstaskor@hotmail.com>
2021-06-24 17:31:13 +0300

    Added Models

---
+++ Armada/Models/Planet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Armada.Models
+{
+    public class Planet
+    {
+        public int PlanetId { get; set; }
+        public string Name { get; set; }
+        public string StarSystem { get; set; }
+        public PlanetType Type { get; set; }
+        public virtual ICollection<Mission> Missions { get; set; }
+
+        public Planet()
+        {
+            Missions = new HashSet<Mission>();
+        }
+    }
+}
